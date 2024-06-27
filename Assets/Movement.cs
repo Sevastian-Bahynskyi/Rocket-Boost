6f031d9635d21fe93899eafd84e4901ee99c0978
Sevastian Bahynskyi <baginski.play@gmail.com>
2024-06-27 19:18:36 +0200

    Implemented rocket movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    [SerializeField] private float mainThrust = 5;
+    [SerializeField] private float rotationThrust = 5;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessInput();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * (mainThrust * Time.deltaTime));
+        }
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        } 
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotationThrust)
+    {
+        rb.freezeRotation = true;
+        gameObject.transform.Rotate(Vector3.forward * (rotationThrust * Time.deltaTime));
+        rb.freezeRotation = false;
+    }
+}
